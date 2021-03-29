0b1fbe4067527748e5fd23c8cd41044cff9a430e
Kpure1000 <ktsq2000@126.com>
2021-03-30 00:04:38 +0800

    init: first commit

---
+++ ChaosTankServer/ChaosTankServer/Network/Network.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Linq;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ChaosTankServer.Network
+{
+    [Serializable]
+    struct DataObj
+    {
+        public int id;
+        public string str;
+    }
+
+    public class Network
+    {
+        public Network()
+        {
+            serverIP = new IPEndPoint(IPAddress.Any, 12345);
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            socket.Bind(serverIP);
+        }
+
+        public void BeginListen()
+        {
+            Console.WriteLine("Begin Listening");
+            try
+            {
+                byte[] reader = new byte[1024];
+                IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+                EndPoint remoteEndPoint = (EndPoint)sender;
+                while (true)
+                {
+                    socket.ReceiveFrom(reader, ref remoteEndPoint);
+                    DataObj dataObj = ReadObjectFromBytes<DataObj>(reader);
+                    Console.WriteLine("ID: {0}, Content: {1}\n", dataObj.id, dataObj.str);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public void Shutdown()
+        {
+            socket.Close();
+        }
+
+        /// <summary>
+        /// ReadObjectFromBytes (Serialize)
+        /// </summary>
+        /// <typeparam name="T">Object Type</typeparam>
+        /// <param name="reader">byte array</param>
+        /// <returns>Object with Target Type</returns>
+        private T ReadObjectFromBytes<T>(byte[] reader)
+        {
+            T reObj;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                ms.Write(reader, 0, reader.Length);
+                ms.Flush();
+                ms.Position = 0;
+
+                BinaryFormatter bf = new BinaryFormatter();
+                reObj = (T)bf.Deserialize(ms);
+            }
+            return reObj;
+        }
+
+        /////////////
+        /// Data
+        /////////////
+
+        private IPEndPoint serverIP;
+        private Socket socket;
+    }
+}
---
+++ ChaosTankServer/ChaosTankServer/ServerMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ChaosTankServer
+{
+    using Network;
+
+    class ServerMain
+    {
+
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Server On");
+
+            Network.Network network = new ChaosTankServer.Network.Network();
+            network.BeginListen();
+        }
+
+    }
+}
